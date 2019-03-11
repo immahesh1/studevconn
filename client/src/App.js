1fceb1fb610a9f0265bb8366ea41d2aac7ce7390
Mahesh Kumar <mahesh_kumar1019@yahoo.in>
2019-03-11 16:12:25 +0530

    navbar and footer done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Navbar from './component/layout/Navbar';
+import Footer from './component/layout/Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>Hello...tesing the react app!!</h1>
+        <Footer />
       </div>
     );
   }
