272bcd148e4208267f7a2b128cbfc8e8664e455e
Mahesh Kumar <mahesh_kumar1019@yahoo.in>
2019-03-12 15:29:24 +0530

    front end login and register without api

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './component/layout/Navbar';
 import Footer from './component/layout/Footer';
+import Landing from './component/layout/Landing';
+import Register from './component/auth/Register';
+import Login from './component/auth/Login';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <h1>Hello...tesing the react app!!</h1>
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/component/layout/Landing.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">
+                  Students' Developer Connector
+                </h1>
+                <p className="lead">
+                  <b>
+                    A social network for students to get in touch with
+                    developers
+                  </b>
+                  <br />
+                  <b>
+                    Create a developer profile/portfolio, share posts and get
+                    help from other developers
+                  </b>
+                </p>
+                <hr />
+                <Link to="register" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link to="login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
