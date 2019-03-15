099e07ee58c7bd29a0cfbf8f6426f2dd3efdace8
Mahesh Kumar <mahesh_kumar1019@yahoo.in>
2019-03-15 20:09:06 +0530

    dashboard lect 50

--- client/src/component/common/InputGroup.js
+++ client/src/component/common/InputGroup.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
-const TextFieldGroup = ({
+const InputGroup = ({
   name,
   placeholder,
   value,
-  label,
   error,
-  info,
+  icon,
   type,
-  onChange,
-  disabled
+  onChange
 }) => {
   return (
-    <div className="form-group">
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text">
+          <i className={icon} />
+        </span>
+      </div>
       <input
-        type={type}
         className={classnames('form-control form-control-lg', {
           'is-invalid': error //is-invalid is only going to come into picture if errors.name exists
         })}
@@ -23,26 +25,23 @@ const TextFieldGroup = ({
         name={name}
         value={value}
         onChange={onChange}
-        disabled={disabled}
       />
-      {info && <small className="form-text form-muted">{info}</small>}
       {error && <div className="invalid-feedback">{error}</div>}
     </div>
   );
 };
 
-TextFieldGroup.propTypes = {
+InputGroup.propTypes = {
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  disabled: PropTypes.string,
-  info: PropTypes.string,
-  error: PropTypes.string,
-  type: PropTypes.string.isRequired
+  icon: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  error: PropTypes.string
 };
 
-TextFieldGroup.defaultProps = {
+InputGroup.defaultProps = {
   type: 'text'
 };
-export default TextFieldGroup;
+export default InputGroup;
--- client/src/component/common/TextAreaFieldGroup.js
+++ client/src/component/common/TextAreaFieldGroup.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
-const TextFieldGroup = ({
+const TextAreaFieldGroup = ({
   name,
   placeholder,
   value,
-  label,
   error,
   info,
-  type,
-  onChange,
-  disabled
+  onChange
 }) => {
   return (
     <div className="form-group">
-      <input
-        type={type}
+      <textarea
         className={classnames('form-control form-control-lg', {
           'is-invalid': error //is-invalid is only going to come into picture if errors.name exists
         })}
@@ -23,7 +19,6 @@ const TextFieldGroup = ({
         name={name}
         value={value}
         onChange={onChange}
-        disabled={disabled}
       />
       {info && <small className="form-text form-muted">{info}</small>}
       {error && <div className="invalid-feedback">{error}</div>}
@@ -31,18 +26,13 @@ const TextFieldGroup = ({
   );
 };
 
-TextFieldGroup.propTypes = {
+TextAreaFieldGroup.propTypes = {
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  disabled: PropTypes.string,
   info: PropTypes.string,
-  error: PropTypes.string,
-  type: PropTypes.string.isRequired
+  error: PropTypes.string
 };
 
-TextFieldGroup.defaultProps = {
-  type: 'text'
-};
-export default TextFieldGroup;
+export default TextAreaFieldGroup;
