b45df55b3fdecf97eb247fe76849219afe9496c1
Mahesh Kumar <mahesh_kumar1019@yahoo.in>
2019-03-19 16:11:58 +0530

    heroku settings

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,5 @@
-//Account using - maheshkumar.mnp@gmail.com
-//username- maheshkumar.mnp@gmail.com
-
-module.exports = {
-  mongoURI:
-    'mongodb://immahesh10:immahesh10@cluster0-shard-00-00-dnlfn.mongodb.net:27017,cluster0-shard-00-01-dnlfn.mongodb.net:27017,cluster0-shard-00-02-dnlfn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
